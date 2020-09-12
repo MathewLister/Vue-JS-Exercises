20e0d174d3c7573da747960e29c5f1870029b70a
MathewLister <listermathew0@gmail.com>
2020-09-12 14:21:49 -0700

    changes

---
+++ Forms/src/Switch.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div
+                id="on"
+                @click="switched(true)"
+                :class="{active: value}">On</div>
+        <div
+                id="off"
+                @click="switched(false)"
+                :class="{active: !value}">Off</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        methods: {
+            switched(isOn) {
+                this.$emit('input', isOn)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #on, #off {
+        width: 40px;
+        height: 20px;
+        background-color: lightgray;
+        padding: 2px;
+        display: inline-block;
+        margin: 10px -2px;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on {
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+    }
+
+    #off {
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+</style>
---
+++ Sharing Components/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                default: 'User Name Here'
+            }
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = 'Luke Skywalker';
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
