f879c9cbfd20bb3295db4e7bc6c1faf996dce402
MathewLister <listermathew0@gmail.com>
2020-09-12 19:19:39 -0700

    gloabl mixins - special case

--- FiltersAndMixins/src/main.js
+++ FiltersAndMixins/src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 
 Vue.filter('toLowercase', function(value){
   return value.toLowerCase();
-})
+});
+
+Vue.mixin({
+  created() {
+    console.log('Global Mixin - Created Hook');
+  }
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
