90a490a7b2e87c2a7f2ba31fdf39b97147e9c0c8
MathewLister <listermathew0@gmail.com>
2020-09-12 18:10:15 -0700

    made custom directives

--- MoreDirectives/src/App.vue
+++ MoreDirectives/src/App.vue
@@ -1,48 +1,63 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
-            </div>
-        </form>
-        <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
-                    </div>
-                </div>
+                <h1>Some Built-in Directives</h1>
+                <p v-text="'Some Text'"></p>
+                <p v-html="'<strong>Some strong Text</strong>'"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Some Custome Directives</h1>
+                <p v-highlight:background.delayed="'red'">Color This</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color This</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+export default {
+    directives: {
+        'local-highlight': {
+            bind(el, binding, vnode) {
+                // el.style.backgroundColor = 'green';
+                // el.style.backgroundColor = binding.value;
+                let delay = 0;
+                if (binding.modifiers['delayed']) {
+                    delay = 2000;
+                }
+                if (binding.modifiers['blink']) {
+                    let mainColor = binding.value.mainColor;
+                    let secondColor = binding.value.secondColor;
+                    let currentColor = mainColor;
+                    setTimeout(() => {
+                        setInterval(() => {
+                            currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                            if (binding.arg = 'background') {
+                                el.style.backgroundColor = currentColor;
+                            } else {
+                                el.style.color = currentColor;
+                            }
+                        }, binding.value.delay);
+                    }, delay);
+
+                } else {
+                    setTimeout(() => {
+                        if (binding.arg = 'background') {
+                            el.style.backgroundColor = binding.value.mainColor;
+                        } else {
+                            el.style.color = binding.value.mainColor;
+                        }
+                    }, delay);
+                }
+            }
+        }
     }
+}
 </script>
 
-<style>
+<style scoped>
+
 </style>
