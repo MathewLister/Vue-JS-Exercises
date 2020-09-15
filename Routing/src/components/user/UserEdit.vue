63d101ce965e96aac5db1ed4bf25f2c29b1e7d84
MathewLister <listermathew0@gmail.com>
2020-09-15 14:27:20 -0700

    added lazy loading components

--- Routing/src/components/user/UserEdit.vue
+++ Routing/src/components/user/UserEdit.vue
@@ -4,8 +4,29 @@
         <h4>Locale: {{ $route.query.locale }}</h4>
         <h5>Analytics: {{ $route.query.q }}</h5>
         <hr>
-        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
         <div style="height: 600px"></div>
         <h2 id="data">Some data......</h2>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            confirmed: false
+        }
+    },
+    beforeRouteLeave(to, from, next) {
+        if (this.confirmed) {
+            next();
+        } else {
+            if (confirm('Are you sure?')) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+}
+</script>
