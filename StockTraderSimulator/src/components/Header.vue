c23db1fd7d420b14efce6a701619b1c6fd2fa72e
MathewLister <listermathew0@gmail.com>
2020-09-15 19:53:20 -0700

    added nav bar

--- StockTraderSimulator/src/components/Header.vue
+++ StockTraderSimulator/src/components/Header.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Header Component</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <router-link class="navbar-brand" to="/">Stock Trader</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <router-link to="/portfolio" class="nav-item" activeClass="active" tag="li"><a class="nav-link">Portfolio</a></router-link>
+                <router-link to="/stocks" class="nav-item" activeClass="active" tag="li"><a class="nav-link">Stocks</a></router-link>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item"><a href="#" class="nav-link">End Day</a></li>
+                <li class="nav-item dropdown">
+                    <a 
+                    class="nav-link dropdown-toggle" 
+                    href="#" id="navbarDropdown" 
+                    role="button" 
+                    data-toggle="dropdown" 
+                    aria-haspopup="true" 
+                    aria-expanded="false">
+                    Save & Load
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Save Data</a>
+                    <a class="dropdown-item" href="#">Load Data</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
