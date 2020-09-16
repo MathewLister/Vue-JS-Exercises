0329201b232b83462a7bb91933e232a843f46cc6
MathewLister <listermathew0@gmail.com>
2020-09-15 21:37:32 -0700

    setting up vuex store

--- StockTraderSimulator/src/components/stocks/Stock.vue
+++ StockTraderSimulator/src/components/stocks/Stock.vue
@@ -1,24 +1,49 @@
 <template>
     <div class="col-sm-6 col-md-4">
         <div class="shadow-sm card" style="width: 18rem;">
-            <div class="card-header text-white bg-success">Name</div>
+            <div class="card-header text-white bg-success">{{ stock.name }} <small>(Price: {{ stock.price }})</small></div>
             <div class="card-body">
-                <h5 class="card-title">Card title
-                    <small>(Price: $128.00)</small>
-                </h5>
                 <div class="col-sm-7 float-left">
                     <input 
                         type="number"
                         class="form-control"
-                        placenolder="Quantity">
+                        placeholder="Quantity"
+                        v-model.number="quantity">
                 </div>
                 <div class="float-right">
-                    <a href="#" class="btn btn-success">Buy</a>
+                    <button
+                        class="btn btn-success"
+                        @click="buyStock"
+                        :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                        >Buy
+                    </button>
                 </div>    
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['stock'],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        buyStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            };
+            console.log(order);
+            this.quantity = 0;
+        }
+    }
+}
+</script>
 
 <style scoped>
     .card {
