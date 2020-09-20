4ab13f864ad0f2e1a2a8ac9a5db5d8f8366c24c2
MathewLister <listermathew0@gmail.com>
2020-09-19 20:54:43 -0700

    change

--- StockTraderSimulator/src/main.js
+++ StockTraderSimulator/src/main.js
@@ -4,11 +4,9 @@ import VueRouter from 'vue-router'
 import { routes } from './routes'
 import store from './store/store'
 import VueResource from 'vue-resource'
-import Meta from 'vue-meta'
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-Vue.use(Meta);
 
 Vue.http.options.root = 'https://vuejs-stock-tra.firebaseio.com/';
 
