51ac548145df12687d09552c87eb12cd43486c06
MathewLister <listermathew0@gmail.com>
2020-09-19 20:13:31 -0700

    vue-meta

--- StockTraderSimulator/src/App.vue
+++ StockTraderSimulator/src/App.vue
@@ -13,8 +13,24 @@
 
 <script>
     import Header from './components/Header.vue'
+    let ROOT_PATH = 'http://vuejs-stock-trader-app-1.s3-website-us-west-2.amazonaws.com/';
 
     export default {
+        data() {
+            return {
+                logo: ROOT_PATH + require('./assets/meta.png')
+            }
+        },
+        metaInfo() {
+            return {
+                meta: [
+                    {property: 'og:title', content: 'VueJS Stock Trading Simulator'},
+                    {property: 'og:descriptions', content: 'A small app I made with VueJS - Hosted with AWS'},
+                    {property: 'og:type', content: 'website'},
+                    {property: 'og:image', content:  this.logo},
+                ]
+            }
+        },
         components: {
             appHeader: Header
         },
